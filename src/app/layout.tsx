3b81a08397e8d4d9314ea20ac7594db0dd5835a1
luis ortiz <ortizluis423@gmail.com>
2025-12-22 22:23:46 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type {Metadata} from 'next';
-import './globals.css';
 import {Providers} from '@/components/providers';
 import {Toaster} from '@/components/ui/toaster';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Bodega Express',
