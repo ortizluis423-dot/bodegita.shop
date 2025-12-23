4a63e939c44c3be998e6f51df849c7acd7373552
luis ortiz <ortizluis423@gmail.com>
2025-12-23 11:34:08 +0000

    Try fixing this error: `Runtime Error: useAuth must be used within an Au

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,4 @@
 import { Header } from "@/components/layout/header";
-import { Providers } from "@/components/providers";
 import { Toaster } from "@/components/ui/toaster";
 
 export default function MainLayout({
@@ -8,12 +7,12 @@ export default function MainLayout({
   children: React.ReactNode;
 }) {
   return (
-    <Providers>
+    <>
       <div className="flex min-h-screen flex-col">
         <Header />
         <main className="flex-1">{children}</main>
       </div>
       <Toaster />
-    </Providers>
+    </>
   );
 }
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,12 +3,16 @@
 import { AuthProvider } from "@/context/AuthContext";
 import { CartProvider } from "@/context/CartContext";
 import { ExchangeRateProvider } from "@/context/ExchangeRateContext";
+import { Toaster } from "./ui/toaster";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
       <ExchangeRateProvider>
-        <CartProvider>{children}</CartProvider>
+        <CartProvider>
+          {children}
+          <Toaster />
+        </CartProvider>
       </ExchangeRateProvider>
     </AuthProvider>
   );
